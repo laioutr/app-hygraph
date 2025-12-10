b3e000b862002f6f77fd42e9ef529227734737f5
Sebastian Langer <sebastian.langer@laioutr.com>
2025-12-10 14:33:01 +0100

    fix: do not transform SVGs in NuxtImage

--- src/runtime/server/hygraph-utils/mediaMapper.ts
+++ src/runtime/server/hygraph-utils/mediaMapper.ts
@@ -1,13 +1,14 @@
 import { MediaImage } from '@laioutr-core/core-types/common';
 
 /**
- * Map a Hygraph asset to a Media object
+ * Map a Hygraph asset to a Media object.
  */
 export const mapHygraphMedia = (asset: any): MediaImage => ({
   type: 'image',
   sources: [
     {
-      provider: 'hygraph',
+      // SVGs are returned as none so they won't be processed by Nuxt Image.
+      provider: asset.mimeType === 'image/svg+xml' ? 'none' : 'hygraph',
       src: asset.url,
       width: asset.width,
       height: asset.height,
